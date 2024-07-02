867775f0f61c6bf50bbe59cb7b8ee86974b2837b
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-02 14:08:45 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,22 +1,11 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
-
-mkdir -p ./storage
-chown -R 1000:1000 ./storage
-
-
-
-
-
-
 MEILI_MASTER_KEY=${MEILI_MASTER_KEY:-`openssl rand -hex 32`}
 JWT_SECRET=${JWT_SECRET:-`openssl rand -hex 32`}
 JWT_REFRESH_SECRET=${JWT_REFRESH_SECRET:-`openssl rand -hex 32`}
 CREDS_KEY=${CREDS_KEY:-`openssl rand -hex 32`}
 CREDS_IV=${CREDS_IV:-`openssl rand -hex 32`}
-
-
 
 cat << EOT >> ./.env
 
